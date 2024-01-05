8c9ba97a922b4c4980eeafcd40d0bcd071187271
ChristopherRdeKoker <christopherdekoker@gmail.com>
2024-01-05 09:16:37 +0200

    created delete functionality for invoices table

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,7 +7,7 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/dashboard/invoices/actions';
+import { createInvoice } from '@/app/dashboard/invoices/action';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
